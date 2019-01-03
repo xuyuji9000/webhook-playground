628a246925a91c9b9a09040c128d12cc1eadaa78
Karl Xu <karl.yogi.xu@gmail.com>
2019-01-03 15:46:42 +0800

    output req

--- webhook.js
+++ webhook.js
@@ -9,6 +9,7 @@ http.createServer(function (req, res) {
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
 
         if (req.headers['x-hub-signature'] == sig) {
+            console.log(req)
             // 1. Get the repository name, branch name
             let repo = '';
             let branch = '';
